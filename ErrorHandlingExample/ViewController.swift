01013d2541913afc2d5e4c49979e6ad2a98f8568
Dan Ursu <dan.ursu@yopeso.com>
2016-02-06 14:21:26 +0100

    Initial Commit

---
+++ ErrorHandlingExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ErrorHandlingExample
+//
+//  Created by Dan Ursu on 06/02/16.
+//  Copyright © 2016 Dan Ursu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
